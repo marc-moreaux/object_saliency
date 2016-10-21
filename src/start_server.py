780e99ec47e6c53ad6b21f4f596ed91f4a9583b1
amazel <amazel@aldebaran-robotics.com>
2016-10-21 19:00:31 +0200

    ca y est on a boucle le process complet, y a plus qu'a test

---
+++ src/start_server.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+###########################################################
+# Start le server qui fait tout ce qu'il faut
+
+# Aldebaran Robotics (c) 2010 All Rights Reserved - This file is confidential.
+###########################################################
+
+import abcdk.socket_receive
+import cnn_object_reco
+
+def runServer():
+    
+    def analyseImage( npImage ):
+      return cnn_object_reco.DetectObject.Instance().detectInBuffer( npImage )
+    
+    s = abcdk.socket_receive.SocketReceiver()
+    nPortNumber = 10000
+    s.run( nPortNumber, analyseImage )
+    #time.sleep( 100    
+# runServer - end
+
+
+runServer()
