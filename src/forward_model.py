88c92b1a40ed9e45f32c3afadac2c3477c13ed8b
Marc Moreaux - cuda <mr.moreaux@gmail.com>
2016-10-24 18:06:17 +0200

    Change to fixed size (224,224) to free one. and add the sum_vis here

--- src/forward_model.py
+++ src/forward_model.py
@@ -35,11 +35,11 @@ class Forward_model:
     self.n_labels  = mod_param.n_labels
     
     # Initialize some tensorflow variables
-    self.images_tf = tf.placeholder( tf.float32, [None, 224, 224, 3], name="images")
+    self.images_tf = tf.placeholder( tf.float32, [None, None, None, 3], name="images")
     self.labels_tf = tf.placeholder( tf.int64, [None], name='labels')
     
     detector = Detector(mod_param)
-    c1,c2,c3,c4,conv5, self.conv6, gap, self.output = detector.inference( self.images_tf )
+    c1,c2,c3,c4,conv5, self.conv6, self.gap, self.output = detector.inference( self.images_tf )
     if re.match(".*CAM._W_S", mod_param.mod_type):
       self.classmap = detector.get_classmap( self.labels_tf, self.conv6 )
     
@@ -71,7 +71,14 @@ class Forward_model:
     feed_dict[self.images_tf] = imgs
     conv6_val, output_val = self.sess.run([self.conv6, self.output],feed_dict=feed_dict)
     preds = output_val
-    named_preds = map(self.to_named_pred, preds)
+
+    # computes the position of the max per axis [-1;1]
+    summed_viz = self.summed_vis(conv6_val)
+    max_pos = lambda arr: (arr.argmax(axis=1) / float(arr.shape[-1]) *2)-1
+    xx   = max_pos( summed_viz.sum(axis=1) )
+    yy   = max_pos( summed_viz.sum(axis=2) )
+
+    named_preds = map(self.to_named_pred, preds, xx, yy)
     return named_preds, conv6_val
   
   def _check_image(self, img):
@@ -79,10 +86,15 @@ class Forward_model:
       img = skimage.color.greytorgb(img)
     if img.shape[-3:] != (224,224,3):
       print "WARNING: FUCKED UP SHAPE, not (224*224*3) !!"
+    if len(img.shape) == 3:
+      img = img.reshape([1,]+list(img.shape))
     return img
   
-  def to_named_pred(self, preds):
-    named_preds = [(self.labels[idx], p) for idx,p in enumerate(preds)]
+  def to_named_pred(self, preds, xx=None, yy=None):
+    if xx == None :
+      named_preds = [(self.labels[idx], p, x, y) for idx,p in enumerate(preds)]
+    else :
+      named_preds = [(self.labels[idx], p, x, y) for idx,(p,x,y) in enumerate(zip(preds, xx, yy))]
     return named_preds
   
   def forward_image(self, img, visualize=False):
@@ -93,7 +105,7 @@ class Forward_model:
         vis -- saliency map to visualize (-1 for best prediction)
     """
     # Reshape image
-    img = img.reshape(1,224,224,3)
+    # img = img.reshape(1,224,224,3)
     tup = self.forward_images(img, visualize)
     return tup[0][0], tup[1]
   
@@ -125,6 +137,33 @@ class Forward_model:
     else :
       return self._visualize_cam_s(imgs)
   
+  def summed_vis(self, vis):
+    # Retrieve model's CAM's properties 
+    # eg: 'CALTECH256.VGG16_CAM5b_S.rmsProp.1e-5' => '5b'
+    name = self.mod_param.get_name()
+    suffix = [ s for s in name.split("_") if "CAM" in s][0][3:]
+    if len(suffix) < 1:
+      suffix = '3'
+    if len(suffix) < 2:
+      suffix += 'a'
+      vis = [vis]
+    
+    filter_sizes = []
+    for idx in range(0, 2, len(suffix)):
+      alphabet_index = ord(suffix[idx+1]) - ord('a') + 1
+      filter_sizes.append((int(suffix[idx]), alphabet_index))
+    
+    # Concatenate all the CAMs to <end_viz>
+    end_viz = None
+    for idx,filter_size in enumerate(filter_sizes):
+      cam_size  = filter_size[0]
+      n_cam     = filter_size[1]
+      new_shape = list(vis[idx].shape[:-1])+[self.n_labels]+[n_cam]
+      m_vis     = vis[idx].reshape(new_shape)
+      m_vis     = m_vis.sum(axis=len(new_shape)-1)
+      end_viz   = m_vis if end_viz == None else sum(end_viz, m_vis)
+    
+    return end_viz
 
 
 
